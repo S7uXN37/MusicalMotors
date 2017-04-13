ac2d6682dbc4484b854b3b9c536f28203716c995
Marc Himmelberger <marc.himmelberger@gmail.com>
2017-04-13 15:05:58 +0200

    Replaced library copy with installation script

--- python-midi-master/build/scripts-2.7/mididumphw.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-"""
-Print a description of the available devices.
-"""
-import midi.sequencer as sequencer
-
-s = sequencer.SequencerHardware()
-
-print s
---
+++ python-midi_install.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt-get install build-essential python-dev python-smbus git
+cd ~
+git clone https://github.com/vishnubob/python-midi.git
+cd python-midi
+sudo python setup.py install
